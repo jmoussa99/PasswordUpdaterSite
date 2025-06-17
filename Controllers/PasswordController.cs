2f7c6151f9d3368f51c08fb6201e644f56c47de6
jmoussa99 <55771230+jmoussa99@users.noreply.github.com>
2025-06-17 12:12:40 -0500

    Add files via upload

---
+++ Controllers/PasswordController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PasswordUpdaterFramework.Models;
+using PasswordUpdaterFramework.Services;
+
+namespace PasswordUpdaterFramework.Controllers
+{
+    public class PasswordController : Controller
+    {
+        private readonly OnBaseService _onBaseService = new OnBaseService();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new PasswordResetModel());
+        }
+
+        [HttpPost]
+        public ActionResult Index(PasswordResetModel model)
+        {
+            if(model.NewPassword.Length < 8)
+            {
+                ViewBag.Message = "Password needs to be at least 8 characters long.";
+                return View(model);
+            }
+            if(!string.Equals((model.NewPassword),(model.repeatPassword)))
+            {
+                ViewBag.Message = "Passwords do not match.";
+                return View(model);
+            }
+            if (string.IsNullOrWhiteSpace(model.Username) || string.IsNullOrWhiteSpace(model.NewPassword))
+            {
+                ViewBag.Message = "Username and password are required.";
+                return View(model);
+            }
+
+            var success = _onBaseService.ChangeUserPassword(model.Username, model.NewPassword, out var message);
+            ViewBag.Message = message;
+            return View(model);
+        }
+    }
+}
---
+++ Services/OnBaseService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Hyland.Unity;
+using System.Configuration;
+
+namespace PasswordUpdaterFramework.Services
+{
+    public class OnBaseService
+    {
+        private readonly string appServerUrl = System.Configuration.ConfigurationManager.AppSettings["OnBase.AppServerUrl"];
+        private readonly string username = System.Configuration.ConfigurationManager.AppSettings["OnBase.Username"];
+        private readonly string password = System.Configuration.ConfigurationManager.AppSettings["OnBase.Password"];
+        private readonly string dataSource = System.Configuration.ConfigurationManager.AppSettings["OnBase.DataSource"];
+
+        private Application Connect()
+        {
+            var authProps = Application.CreateOnBaseAuthenticationProperties(appServerUrl, username, password, dataSource);
+            authProps.LicenseType = LicenseType.Default;
+            return Application.Connect(authProps);
+        }
+
+        public bool ChangeUserPassword(string userName, string newPassword, out string message)
+        {
+            try
+            {
+                using (var app = Connect())
+                {
+                    var user = app.Core.GetUser(userName);
+                    if (user == null)
+                    {
+                        message = "User not found.";
+                        return false;
+                    }
+
+                    if(user.IsLocked()) app.Core.UserAdministration.UnlockUserAccount(user);
+                    var userProps = app.Core.UserAdministration.CreateEditableUserProperties();
+
+                    userProps.UserName = userName;
+                    userProps.Password = newPassword;
+                    userProps.ChangePasswordAtLogin = false;
+
+                    app.Core.UserAdministration.ChangeUserProperties(user, userProps);
+                    message = "Password updated successfully.";
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                message = $"Error: {ex.Message}";
+                return false;
+            }
+        }
+    }
+}
